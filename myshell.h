98a6ba75dba1f6f5ed2c0545d0c3009ec5032da2
BlueBug <zihan9701@gmail.com>
2019-10-02 13:30:07 -0400

    add prototype mycd()

---
+++ myshell.h
@@ -0,0 +1,24 @@
+#ifndef MYSHELL_H_
+#define MYSHELL_H_
+
+struct commandLine
+{
+    char* commandName;
+    char* arg[100]; // all arguments
+    int argCount; // the number of arguments
+    char* file; 
+    int inputMod;  // < or << read in
+    int outputMod; // > or >> write out
+    int background; // & at the end?
+    int parallel;
+    struct commandLine* one;
+};
+typedef struct commandLine command;
+
+extern char* environ[];
+
+void mycd(command* cmd);
+
+
+
+#endif
