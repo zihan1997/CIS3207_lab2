ddeee1052fb85a867f28942da7aeaab87ab7aa30
BlueBug <zihan9701@gmail.com>
2019-10-08 16:24:09 -0400

    prototypes of input and output added

--- myshell.h
+++ myshell.h
@@ -11,12 +11,22 @@ struct commandLine
     int outputMod; // > or >> write out
     int background; // & at the end?
     int parallel;
+    int pipe;
     struct commandLine* one;
 };
 typedef struct commandLine command;
 
 extern char* environ[];
 
+void initialize(command* cmd);
+void printCommand(command* cmd);
+void parsePipe(command* cmd, char* line);
+void parseSpaces(command* cmd, char* line);
+void parseBackground(command* cmd, char* line);
+void parseParallel(command* cmd, char* line);
+void input_redirection(command* cmd, char* line);
+void output_redirection(command* cmd, char* line);
+void parseLine(command* cmd, char* line);
 void mycd(command* cmd);
 void my_clr();
 void my_dir(command* cmd);
